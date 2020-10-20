87a93fedb333e647fe2ccee8e958e97f4b16cbf1
abhishekwl <abhishekwl@protonmail.com>
2020-10-20 22:13:49 +0530

    Update

--- sa_mw/settings.py
+++ sa_mw/settings.py
@@ -88,12 +88,20 @@ WSGI_APPLICATION = 'sa_mw.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elast1',
+        'USER': 'tvsadmn',
+        'PASSWORD': 'passwordtvst',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 """
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'elast1',
